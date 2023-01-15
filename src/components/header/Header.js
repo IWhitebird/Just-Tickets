fbe1dce2e047429f0fcdc9140ba82df2ac31cb93
Shreyas <patangeshreyas@gmail.com>
2023-01-15 19:31:28 +0530

    logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,15 @@
 import React from "react"
 import "./Header.css"
 import { Link } from "react-router-dom"
+import LOGO from "./LOGO" 
 
 const Header = () => {
     return (
         <div className="header">
             <div className="headerLeft">
-                <Link to="/"><img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" /></Link>
+                <Link to="/"><img className="header__icon" src={LOGO} /></Link>
                 <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
-                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top WATCHEDASDSADSADssS</span></Link>
+                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Watched</span></Link>
                 <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
             </div>
         </div>
