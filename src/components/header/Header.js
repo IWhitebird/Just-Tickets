05891a82e765e7170d23fad1a1a85b4ad8efea42
Shreyas <patangeshreyas@gmail.com>
2023-01-15 19:44:59 +0530

    F

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,12 @@
 import React from "react"
 import "./Header.css"
 import { Link } from "react-router-dom"
-import LOGO from "./LOGO" 
 
 const Header = () => {
     return (
         <div className="header">
             <div className="headerLeft">
-                <Link to="/"><img className="header__icon" src={LOGO} /></Link>
+                <Link to="/"><img className="header__icon" src="https://cdn.discordapp.com/attachments/1063718816363851868/1064173558261092484/Untitled-1.png" /></Link>
                 <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
                 <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Watched</span></Link>
                 <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
