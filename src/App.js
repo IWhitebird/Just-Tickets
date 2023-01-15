7e2ed4cb29bd136fb13fcabe704338708caf30db
Xeonen24 <108137169+Xeonen24@users.noreply.github.com>
2023-01-15 21:35:11 +0530

    fix log

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ function App() {
                 <Route index element={<Home />}></Route>
                 <Route path="movie/:id" element={<Movie />}></Route>
                 <Route path="movies/:type" element={<MovieList />}></Route>
+                <Route path="/pages/login" element={<Login />}></Route>
             </Routes> 
-            <Login />
             <Footer />
         </Router>
     </div>
